d73b00a8fc7e75bcf72486c9623306393d7c4e57
kuojianghu <784818329@qq.com>
2016-06-23 09:42:59 +0800

    Create my.h
    add client of class of MFC

---
+++ my.h
@@ -0,0 +1,19 @@
+#ifndef MY_H__
+#define MY_H__
+
+#include "mfc.h"
+#include <iostream>
+
+class CmyWinApp : public CwinApp{
+public:
+  ~CmyWinApp(){}
+  CmyWinApp(){}
+};
+
+class CmyFrameWnd : public CframeWnd{
+public:
+  ~CmyFrameWnd(){}
+  CmyFrameWnd(){}
+};
+
+#endif
