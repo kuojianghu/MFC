0491ee4ddc6b7ae98ea01d3faa8574293eb49083
kuojianghu <784818329@qq.com>
2016-06-23 09:30:35 +0800

    Update mfc.h
    add a class of MFC class

--- mfc.h
+++ mfc.h
@@ -23,3 +23,37 @@ public:
   ~CwinThread(){}
   CWinThread(){}
 };
+
+class CwinApp : public CwinThread{
+public:
+  ~CwinApp(){ };
+  CwinApp(){}
+};
+
+class Cdocument : public CmdTarget{
+public:
+  ~Cdocument(){}
+  Cdocument(){}
+};
+
+class Cwnd : public CmdTarget{
+public:
+  ~Cwnd(){  }
+  Cwnd(){}
+};
+
+class CframeWnd : public Cwnd{
+public:
+  ~CframeWnd(){}
+  CframeWnd(){}
+};
+
+class Cview : public Cwnd{
+public:
+  ~Cview(){}
+  Cview(){}
+};
+
+CwinApp AfxGetApp();
+
+#endif
