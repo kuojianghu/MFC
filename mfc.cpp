56ac1272a8ad075a15d31e7dd11b9b05715c4c69
kuojianghu <784818329@qq.com>
2016-06-24 08:41:18 +0800

    Update mfc.cpp

--- mfc.cpp
+++ mfc.cpp
@@ -3,6 +3,20 @@
 
 extern CwinMyApp theApp();
 
+static char szCobject[] = "Cobject";
+
+struct CrunttimeClass Cobject::classObject = {
+  szObject, sizeof(Cobject), 0xffff, NULL, NULL, NULL
+};
+
+static AFX_CLASSINIT _init_Cobject(&Cobject::classCobject);
+CruntimeClass* CruntimeClass::pFristClass = NULL;
+
+AFX_CLASSINIT::AFX_CLASSINIT(CruntimeClass* pNewClass){
+  pNewClass->m_pNextClass = CruntimeClass::pFristClass;
+  CruntimeClass::pFristClass = pNewClassl;
+}
+
 CwinApp* AfxGetApp(){
   return theApp.m_pCurrentWinApp;  
 }
